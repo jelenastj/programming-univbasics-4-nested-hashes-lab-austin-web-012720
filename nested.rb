d558ec4620364af876acde0705fad3af67b6b5b2
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-03 01:49:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,11 @@ end
 def adding_matz
 # add the following information to the top level of programmer_hash = {
   
-
-:yukihiro_matsumoto => {
-  :known_for => "Ruby",
+    :yukihiro_matsumoto => {
+    :known_for => "Ruby",
     :languages => ["LISP", "C"]
 }
+end
 
 	programmer_hash = 
  		{
