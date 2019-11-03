b193d986c9eab252cb1b52597a68c36601555a69
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-03 01:11:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -6,10 +6,12 @@ def hopper
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
+        
         :alan_kay => {
           :known_for => "Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
+        
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => ["C"]
