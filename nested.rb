1efd69dc2c37ebb4b16047062cc739057314273a
Jelena Stjepanovic <jelena.stjepanovic@gmail.com>
2019-11-03 02:08:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -62,6 +62,22 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash 
+
+programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
 programmer_hash.merge(:yukihiro_matsumoto => {
    :known_for => "Ruby",
     :languages => ["LISP, C"]
